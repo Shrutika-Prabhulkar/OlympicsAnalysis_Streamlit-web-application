a175e9cfe4b14127e7973e5102dc191a4e8b7b48
ShrutikaP <54306018+Shrutika-Prabhulkar@users.noreply.github.com>
2024-12-17 02:11:44 +0530

    Add files via upload

---
+++ olympics-analysis-webapp/preprocessor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def preprocess(df,region_df):
+        # Filtering for Summer olympics
+    df= df[df['Season'] == 'Summer']
+    #Merge with region_df
+    df = df.merge(region_df, how='left', on='NOC')
+    #Dropping Duplicates
+    df.drop_duplicates(inplace= True)
+    # One hot encoding medals
+    df =pd.concat([df,pd.get_dummies(df['Medal'])],axis=1)
+    return df
+
